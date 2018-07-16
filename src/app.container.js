225b3ca4b7922eb5d769d2d1c267de362775f3d1
lenconda <1053464288@qq.com>
2018-07-16 19:48:06 +0800

    💡 添加iOS splash

--- src/app.container.js
+++ src/app.container.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import RNSplashScreen from 'react-native-splash-screen'
 
 import reducers from './redux/reducers'
 import AppRouter from './app.router'
@@ -8,11 +9,19 @@ import AppRouter from './app.router'
 const store = createStore(reducers)
 
 export default class AppContainer extends React.Component {
+
+  componentDidMount() {
+    RNSplashScreen.hide()
+  }
+
   render() {
+
     return (
       <Provider store={store}>
         <AppRouter />
       </Provider>
     )
+
   }
+
 }
