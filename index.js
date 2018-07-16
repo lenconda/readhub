082cbf16f7e78cbe1161f2ec44ecb7f52e314fb6
lenconda <1053464288@qq.com>
2018-07-16 23:05:23 +0800

    💥 解决了YellowBox报错的问题

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { AppRegistry } from 'react-native';
-import AppContainer from './src/app.container';
-import { YellowBox } from 'react-native';
+import { AppRegistry } from 'react-native'
+import AppContainer from './src/app.container'
+import { YellowBox } from 'react-native'
 
-AppRegistry.registerComponent('ReadHub', () => AppContainer);
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+AppRegistry.registerComponent('ReadHub', () => AppContainer)
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
+YellowBox.ignoreWarnings(['Class RCTCxxModule'])
