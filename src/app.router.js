b3a2b8495564fe88f1c3087d9c53d7333fc18f12
lenconda <1053464288@qq.com>
2018-07-16 23:42:13 +0800

    🔥 清理多余代码

--- src/app.router.js
+++ src/app.router.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { Actions, Scene, Router, Stack } from 'react-native-router-flux'
 import {StyleSheet} from "react-native";
 
-import Login from './pages/Login'
-import Register from './pages/Register'
 import IndexContainer from './container/IndexContainer'
 import TopicContainer from './container/TopicContainer'
 import WebContainer from './container/WebContainer'
@@ -19,8 +17,6 @@ export default class AppRouter extends Component {
       >
         <Stack key={'root'}>
           <Scene key={'index'} component={IndexContainer} title={'话题'}></Scene>
-          <Scene key={'login'} component={Login} title={'Login'}></Scene>
-          <Scene key={'register'} component={Register} title={'Register'}></Scene>
           <Scene key={'topicContainer'} component={TopicContainer} title={'话题详情'}></Scene>
           <Scene key={'newsContainer'} component={WebContainer} title={'加载中...'}></Scene>
           <Scene key={'careerContainer'} component={CareerContainer} title={'招聘详情'}></Scene>
