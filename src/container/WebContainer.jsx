bacf83d854cd742cf47e356c0461e90b30683932
lenconda <1053464288@qq.com>
2018-07-18 00:33:25 +0800

    🔥 修改WebView组件，仍存在问题（Android）

--- src/container/WebContainer.jsx
+++ src/container/WebContainer.jsx
@@ -18,18 +18,18 @@ export default class WebContainer extends Component {
 
   render() {
 
-    const patchPostMessageFunction = function() {
-      var originalPostMessage = window.postMessage
-      var patchedPostMessage = function(message, targetOrigin, transfer) {
-        originalPostMessage(message, targetOrigin, transfer)
-      }
-      patchedPostMessage.toString = function() {
-        return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage')
-      }
-      window.postMessage = patchedPostMessage
-    }
+    const patchPostMessageFunction=function(){
+      var originalPostMessage=window.postMessage;
+      var patchedPostMessage=function(message,targetOrigin,transfer){
+        originalPostMessage(message,targetOrigin,transfer);
+      };
+      patchedPostMessage.toString=function(){
+        return String(Object.hasOwnProperty).replace('hasOwnProperty','postMessage');
+      };
+      window.postMessage=patchedPostMessage;
+    };
 
-    const patchPostMessageJsCode = '(' + String(patchPostMessageFunction) + ')();window.postMessage(document.title, "*")'
+    const patchPostMessageJsCode = '(' + String(patchPostMessageFunction) + ')();\nwindow.postMessage(document.title,\'*\');'
 
     return (
       <View style={{ flex: 1 }}>
